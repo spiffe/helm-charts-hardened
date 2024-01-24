0f54c638d6248b8ee346f20d9c3775ec46ceb6a9
Kevin Fox <Kevin.Fox@pnnl.gov>
2024-01-24 07:05:46 -0800

    Always upgrade test from the previous major release
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- examples/production/run-tests.sh
+++ examples/production/run-tests.sh
@@ -2,7 +2,7 @@
 
 set -xe
 
-UPGRADE_VERSION=v0.15.1
+UPGRADE_VERSION=$(git tag -l 'spire-0.*' | awk -F. '{print $2}' | sort -n | tail -n 1 | sed 's/^/v0./; s/$/.0/')
 UPGRADE_REPO=https://spiffe.github.io/helm-charts-hardened
 
 SCRIPT="$(readlink -f "$0")"
