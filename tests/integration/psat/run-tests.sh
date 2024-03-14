2f4adcb83706f48e52b1e035575d7320493d6637
Kevin Fox <Kevin.Fox@pnnl.gov>
2024-03-14 02:32:43 -0700

    Fix test
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- tests/integration/psat/run-tests.sh
+++ tests/integration/psat/run-tests.sh
@@ -64,6 +64,8 @@ kubectl --kubeconfig "${SCRIPTPATH}/kubeconfig" create configmap -n spire-system
 
 helm upgrade --kubeconfig "${SCRIPTPATH}/kubeconfig" --install --create-namespace --namespace spire-mgmt spire-crds charts/spire-crds
 kubectl --kubeconfig "${SCRIPTPATH}/kubeconfig" apply -f "${SCRIPTPATH}/sodp-clusterspiffeid.yaml"
+helm upgrade --install --create-namespace --namespace spire-server --values "${SCRIPTPATH}/child-values.yaml" \
+  spire charts/spire
 
 helm upgrade --install --create-namespace --namespace spire-server --values "${SCRIPTPATH}/values.yaml" \
   --wait spire charts/spire --set "spire-server.kubeConfigs.other.kubeConfigBase64=$KCB64"
