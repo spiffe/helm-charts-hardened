6e7ceaecf1e37dac0e4b7f72221163d220eaf825
Kevin Fox <Kevin.Fox@pnnl.gov>
2024-03-24 12:38:21 -0700

    Add security cluster example
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- examples/nested-security/run-tests.sh
+++ examples/nested-security/run-tests.sh
@@ -68,7 +68,7 @@ kubectl get configmap -n kube-system coredns -o yaml | grep production.other || 
 kubectl rollout restart -n kube-system deployment/coredns
 kubectl rollout status -n kube-system -w --timeout=1m deploy/coredns
 
-for cluster in child other; do
+for cluster in child; do
   KC="${SCRIPTPATH}/kubeconfig-${cluster}"
 
   kind create cluster --name "${cluster}" --kubeconfig "${SCRIPTPATH}/kubeconfig-${cluster}" --config "${SCRIPTPATH}/.test-files/${cluster}-kind-config.yaml"
@@ -77,7 +77,7 @@ for cluster in child other; do
 
   helm upgrade --kubeconfig "${KC}" --install --create-namespace --namespace spire-mgmt spire-crds charts/spire-crds
   helm upgrade --kubeconfig "${KC}" --install --namespace spire-mgmt --values "${COMMON_TEST_YOUR_VALUES},${SCRIPTPATH}/child-values.yaml" \
-    --set "global.spire.upstreamSpireAddress=spire-server.production.other" \
+    --set "global.downstream-spire-agent.server.address: spire-server.changeme=spire-server.production.other" \
     --set "global.spire.namespaces.create=true" \
     --set "global.spire.clusterName=${cluster}" \
     spire charts/spire-nested
@@ -89,26 +89,19 @@ for cluster in child other; do
 done
 
 docker exec -i child-control-plane bash -c 'kubeadm kubeconfig user --client-name=spire-root' > "${SCRIPTPATH}/child-spire-root.kubeconfig"
-docker exec -i other-control-plane bash -c 'kubeadm kubeconfig user --client-name=spire-root' > "${SCRIPTPATH}/other-spire-root.kubeconfig"
 CHILD_KCB64="$(base64 < "${SCRIPTPATH}/child-spire-root.kubeconfig" | tr '\n' ' ' | sed 's/ //g')"
-OTHER_KCB64="$(base64 < "${SCRIPTPATH}/other-spire-root.kubeconfig" | tr '\n' ' ' | sed 's/ //g')"
 
 helm upgrade --install --create-namespace --namespace spire-mgmt --values "${COMMON_TEST_YOUR_VALUES},${SCRIPTPATH}/root-values.yaml" \
   --wait spire charts/spire-nested \
   --set "global.spire.namespaces.create=true" \
   --set "global.spire.ingressControllerType=ingress-nginx" \
-  --set "external-spire-server.kubeConfigs.child.kubeConfigBase64=${CHILD_KCB64}" \
-  --set "external-spire-server.kubeConfigs.other.kubeConfigBase64=${OTHER_KCB64}"
+  --set "external-spire-server.kubeConfigs.child.kubeConfigBase64=${CHILD_KCB64}"
 
-for cluster in child other; do
+for cluster in child; do
   KC="${SCRIPTPATH}/kubeconfig-${cluster}"
   kubectl --kubeconfig "${KC}" get configmap -n spire-system spire-bundle-upstream -o yaml
-  kubectl --kubeconfig "${KC}" rollout restart daemonset spire-agent-upstream -n spire-system
-  kubectl --kubeconfig "${KC}" rollout restart statefulset spire-internal-server -n spire-server
   kubectl --kubeconfig "${KC}" rollout restart daemonset spire-agent-downstream -n spire-system
   kubectl --kubeconfig "${KC}" rollout restart deployment spiffe-oidc-discovery-provider -n spire-server
-  kubectl --kubeconfig "${KC}" rollout status daemonset spire-agent-upstream -n spire-system --timeout 60s || kubectl logs --kubeconfig "${KC}" daemonset/spire-agent-upstream -n spire-system --prefix --all-containers=true
-  kubectl --kubeconfig "${KC}" rollout status statefulset spire-internal-server -n spire-server --timeout 60s || kubectl logs --kubeconfig "${KC}" statefulset/spire-internal-server -n spire-server --prefix --all-containers=true
   kubectl --kubeconfig "${KC}" rollout status daemonset spire-agent-downstream -n spire-system --timeout 60s || kubectl logs --kubeconfig "${KC}" daemonset/spire-agent-downstream -n spire-system --prefix --all-containers=true
   kubectl --kubeconfig "${KC}" rollout status deployment spiffe-oidc-discovery-provider -n spire-server --timeout 60s || kubectl logs --kubeconfig "${KC}" deployment/spiffe-oidc-discovery-provider -n spire-server --prefix --all-containers=true
 
@@ -133,5 +126,4 @@ fi
 helm test --namespace spire-mgmt spire
 
 helm test --kubeconfig "${SCRIPTPATH}/kubeconfig-child" --namespace spire-mgmt spire
-helm test --kubeconfig "${SCRIPTPATH}/kubeconfig-other" --namespace spire-mgmt spire
 
