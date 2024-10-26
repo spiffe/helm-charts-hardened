f3cd0f2ea3a77a1257f4ac14ce362a05745ad95f
Kevin Fox <Kevin.Fox@pnnl.gov>
2024-10-25 19:15:11 -0700

    Fix perms
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- tests/integration/spiffe-step-ssh/run-tests.sh
+++ tests/integration/spiffe-step-ssh/run-tests.sh
@@ -161,6 +161,10 @@ curl https://spiffe-step-ssh-fetchca.production.other -s --cacert /tmp/ca.pem
 
 sudo systemctl start spiffe-step-ssh@main
 
+# This is bad, but only for testing. Don't do this at home
+sudo chmod 777  "/var/run/spiffe/"
+sudo chmod 777  "/var/run/spiffe/step-ssh/"
+sudo chmod 777  "/var/run/spiffe/step-ssh/main/"
 common_test_file_exists "/var/run/spiffe/step-ssh/main/ssh_host_rsa_key-cert.pub"
 
 kubectl get configmap spiffe-step-ssh-certs -o 'go-template={{ index .data "ssh_host_ca_key.pub" }}' | sed '/^$/d; s/^/@cert-authority *.production.other /' | sudo -u spiffe-test dd of=/home/spiffe-test/.ssh/known_hosts
