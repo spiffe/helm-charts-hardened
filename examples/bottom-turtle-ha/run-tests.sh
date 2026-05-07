9eb625d08a8ca2dbec356c267e8909d349f54f33
Kevin Fox <Kevin.Fox@pnnl.gov>
2026-05-07 07:39:11 -0700

    Fix path
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- examples/bottom-turtle-ha/run-tests.sh
+++ examples/bottom-turtle-ha/run-tests.sh
@@ -69,6 +69,8 @@ sudo systemctl start spire-server@a spire-server@b spire-controller-manager@a sp
 sudo systemctl status spire-server@a
 sudo systemctl status spire-server@b
 
+sleep 10
+
 sudo ls /run/spire/server/sockets/a/private /run/spire/server/sockets/a /run/spire/server
 
 export JOIN_TOKEN_A=$(sudo spire-server token generate -spiffeID spiffe://example.org/agentA -socketPath /run/spire/server/sockets/a/private/api.sock | awk '{print "\""$2"\""}')
