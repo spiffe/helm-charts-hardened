3207cb75589178b4f7f71e45a60f5c7e9ec2b326
Kevin Fox <Kevin.Fox@pnnl.gov>
2026-05-09 07:36:48 -0700

    Test some more bits
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- examples/bottom-turtle-ha/run-tests.sh
+++ examples/bottom-turtle-ha/run-tests.sh
@@ -81,7 +81,7 @@ wait_for_healthcheck() {
   local count=0
   while [ "$count" -lt "$timeout" ]; do
       rc=0
-      "$app" healthcheck -socketPath "$socket" || rc=$?
+      sudo "$app" healthcheck -socketPath "$socket" || rc=$?
       if [ "$rc" -eq 0 ]; then
         return 0
       fi
