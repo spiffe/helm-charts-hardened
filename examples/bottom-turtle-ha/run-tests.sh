28525a9d4d20539015c35be34897a1328d715822
Kevin Fox <Kevin.Fox@pnnl.gov>
2026-05-10 05:21:10 -0700

    Test some more bits
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- examples/bottom-turtle-ha/run-tests.sh
+++ examples/bottom-turtle-ha/run-tests.sh
@@ -282,6 +282,7 @@ helm upgrade --install --namespace spire-mgmt --values "${COMMON_TEST_YOUR_VALUE
   --set "global.spire.ingressControllerType=ingress-nginx" \
   -f test-a-values.yaml
 
+kubectl get pods -A
 kubectl get ingress -A
 curl -k --resolve "oidc-discovery.production.other:443:$IP" "https://oidc-discovery.production.other/.well-known/openid-configuration" -s --fail
 helm test --namespace spire-mgmt spire
