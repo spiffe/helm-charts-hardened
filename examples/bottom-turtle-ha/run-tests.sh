7be8b28908c8953096db2c5799cc518802fdc4b5
Kevin Fox <Kevin.Fox@pnnl.gov>
2026-06-30 18:23:45 -0700

    Update
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- examples/bottom-turtle-ha/run-tests.sh
+++ examples/bottom-turtle-ha/run-tests.sh
@@ -302,8 +302,8 @@ curl -k --resolve "oidc-discovery.production.other:443:$IP" "https://oidc-discov
 kubectl apply -f "${SCRIPTPATH}/test-job.yaml"
 kubectl wait --for=condition=complete --timeout=60s job/test && \
 TOKEN=$(kubectl logs job/test)
-curl -f -H "Authorization: Bearer ${TOKEN}" -X POST --resolve "#spire-identity-exchange-a-rest.production.other:443:$IP" "https://#spire-identity-exchange-a-rest.production.other/api/v1/svid/k8s_psat/x509" -k -sS -q
-curl -f -H "Authorization: Bearer ${TOKEN}" -X POST --resolve "#spire-identity-exchange-b-rest.production.other:443:$IP" "https://#spire-identity-exchange-b-rest.production.other/api/v1/svid/k8s_psat/x509" -k -sS -q
+curl -f -H "Authorization: Bearer ${TOKEN}" -X POST --resolve "spire-identity-exchange-a-rest.production.other:443:$IP" "https://spire-identity-exchange-a-rest.production.other/api/v1/svid/k8s_psat/x509" -k -sS -q
+curl -f -H "Authorization: Bearer ${TOKEN}" -X POST --resolve "spire-identity-exchange-b-rest.production.other:443:$IP" "https://spire-identity-exchange-b-rest.production.other/api/v1/svid/k8s_psat/x509" -k -sS -q
 
 #Test out running only on side b since we know already only both servers work together, and that only side a works if we made it this far.
 helm delete -n spire-mgmt spire-a
