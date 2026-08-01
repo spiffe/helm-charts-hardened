745d64d64ed438662c4c66750f77e0e0fc8590e6
Kevin Fox <Kevin.Fox@pnnl.gov>
2026-08-01 16:15:12 -0700

    Update name
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- examples/bottom-turtle-ha/run-tests.sh
+++ examples/bottom-turtle-ha/run-tests.sh
@@ -95,7 +95,7 @@ teardown() {
   kubectl describe daemonset pods -n spire-system || true
   kubectl get configmap -n spire-system || true
   kubectl get configmap -n spire-system spire-a-agent-downstream -o yaml || true
-  kubectl get endpoints -n svc-server -o yaml || true
+  kubectl get endpoints -n spire-server -o yaml || true
 
   print_helm_releases
 
